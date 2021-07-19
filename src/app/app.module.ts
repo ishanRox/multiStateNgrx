897685354f299cb31dff3a41ff2dbd5bdf42badc
Drone <roxishan8@gmail.com>
2021-07-19 08:48:58 +0530

    added multistate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { ShowComponent } from './number-view/show/show.component';
 import { PostComponent } from './post-view/post/post.component';
 import { PostviewComponent } from './post-view/postview/postview.component';
 import { postReducer } from './ngrx/reducer/postreducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,12 @@ import { postReducer } from './ngrx/reducer/postreducer';
     PostviewComponent
   ],
   imports: [
-    StoreModule.forRoot({ numberSharedState: numberReducer ,postSharedState:postReducer}),
+    StoreModule.forRoot({ numberSharedState: numberReducer, postSharedState: postReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
+
     BrowserModule,
     AppRoutingModule
   ],
